9e44dc199213a3d758bcc20c6b4cd08e05417a47
Michael Hunger <github@jexp.de>
2025-07-07 15:41:38 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ export GOOGLE_GENAI_USE_VERTEXAI=FALSE
 # ollama pull qwen3:latest
 # docker run -p 7687:7687 -p 7474:7474 -e NEO4J_PLUGINS='["apoc"]' -e NEO4J_AUTH=neo4j/password neo4j
 #export NEO4J_URI=bolt://localhost
-export NEO4J_URI=neo4j+s://fda2b1c4.databases.neo4j.io
+export NEO4J_URI=neo4j+s://1a1c5a65.databases.neo4j.io
 export NEO4J_URL=$NEO4J_URI
 export NEO4J_USERNAME=neo4j
 export NEO4J_DATABASE=neo4j
-export NEO4J_PASSWORD="L8ZkmVZFj_rk3WwVKc73EEXsEk0Y-vEyxzoQAhm3DHg"
-# pip install uv
+export NEO4J_PASSWORD="StzTyZTC1oaWRnh76hKfkpdRMZl_qk-VdK6d6MLYZtc"
+pip install uv
 # or curl -LsSf https://astral.sh/uv/install.sh | sh
 uv sync
 uv run adk web
